de497cb76f2ec509ffc1ba85d83ab10f5c5e84c2
sarath-auto <sarath@autoaigis.com>
2026-06-13 13:56:39 -0700

    Forge heal: fix /gigledger-mvp (9ab8183f)

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,6 @@
-import { type ClassValue, clsx } from \"clsx\"\nimport { twMerge } from \"tailwind-merge\"\n\nexport function cn(...inputs: ClassValue[]) {\n  return twMerge(clsx(inputs))\n}\n
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
